63a9eaeb40a4b32e89d902afddd4d867725005e6
Abigail Rettew <abigailrettew@Abigails-MBP-2.lan1>
2020-05-03 15:24:54 -0400

    Added raycaster/ability to switch between room and desktop

--- src/app.js
+++ src/app.js
@@ -6,14 +6,25 @@
  * handles window resizes.
  *
  */
-import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
+import { 
+    WebGLRenderer, 
+    PerspectiveCamera,
+    Vector3,
+    Vector2,
+    Raycaster 
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { Room, Desktop } from 'scenes';
 
 // Initialize core ThreeJS components
-const scene = new Room();
+var scene = new Room();
 const camera = new PerspectiveCamera();
 const renderer = new WebGLRenderer({ antialias: true });
+
+//  Keep track of scenes
+var scenes = [];
+scenes['room'] = scene;
+scenes['desktop'] = new Desktop();
 
 // Set up camera
 camera.position.set(0, 3, 8);
@@ -34,6 +45,35 @@ controls.enablePan = false;
 controls.minDistance = 4;
 controls.maxDistance = 16;
 controls.update();
+
+//  Set up raycaster
+const raycaster = new Raycaster();
+var mouse = new Vector2();
+const onMouseMove = (event) => {
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+window.addEventListener('mousemove', onMouseMove, false);
+
+//  Use raycaster
+const onClick = (event) => {
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    //  Let individual scenes handle click event for now
+    if (scene.onClick != undefined) {
+        const res = scene.onClick(event, intersects);
+        if (res != undefined) {
+            //  Scenes tell app to switch scenes by returning strings?
+            switchScene(res);
+        }
+    }
+}
+window.addEventListener('click', onClick, false);
+
+//  Switch scene
+const switchScene = (newScene) => {
+    scene = scenes[newScene];
+}
 
 // Render loop
 const onAnimationFrameHandler = (timeStamp) => {
--- src/components/scenes/Desktop.js
+++ src/components/scenes/Desktop.js
@@ -6,6 +6,11 @@ class Desktop extends Scene {
 
         this.background = new Color(0x080808);
     }
+
+    onClick(event, results) {
+        //  Return to room on any click
+        return 'room';
+    }
 }
 
 export { Desktop };
