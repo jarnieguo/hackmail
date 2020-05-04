1d21af417774bc33a7f51edeb7341eaea52617d3
zexin <zkoh@princeton.edu>
2020-05-04 16:36:34 -0400

    pop bubbles, keep score

--- src/components/stages/TextBubbleGame/TextBubbleGame.js
+++ src/components/stages/TextBubbleGame/TextBubbleGame.js
@@ -7,16 +7,33 @@ import BUBBLE from './bubble.png';
 
 // https://pixijs.io/examples/#/interaction/custom-hitarea.js
 const bubbleIcon = PIXI.Texture.from(BUBBLE);
-const GRAVITY = 0.6;
+const GRAVITY = 0.4;
 
 class Bubble {
-  constructor() {
+  constructor(parent) {
+    this.parent = parent;
+
     this.sprite = new PIXI.Sprite(bubbleIcon);
 
     this.sprite.position.set(Math.random() * window.innerWidth, window.innerHeight + 10);
 
     this.xVel = (Math.random() - 0.5) * 4;
-    this.yVel = -1 * Math.random() * 40;
+    this.yVel = -1 * Math.random() * 30;
+
+    this.sprite.buttonMode = true;
+    this.sprite.interactive = true;
+
+    this.popped = false;
+
+    this.sprite.on('pointerdown', (event) => this.onClick(this.sprite));
+  }
+
+  // pop
+  onClick(object) {
+      object.tint = 0x333333;
+      this.popped = true;
+
+      this.parent.score += 1;
   }
 
   update() {
@@ -36,6 +53,13 @@ class TextBubbleGame extends Game {
         this.setLabel("text bubble game");
 
         this.bubbles = [];
+        this.score = 0;
+
+        this.scoreText = new PIXI.Text(('Score: ' + this.score));
+        //  {fontFamily : 'Arial', fontSize: 20, fill : 0x, align : 'center'}
+        this.scoreText.position.set(0, 70);
+
+        this.objects.push(this.scoreText);
 
         this.initObjects();
     }
@@ -58,40 +82,44 @@ class TextBubbleGame extends Game {
     }
 
     initBubble() {
-      let bub = new Bubble();
+      let bub = new Bubble(this);
 
       this.bubbles.push(bub);
       //this.objects.push(bub.sprite);
       this.parent.stage.addChild(bub.sprite);
     }
 
-    // util: is bubble still in screen?
-    inScreen(bubble) {
+    // util: is bubble still on screen AND not popped
+    isAlive(bubble) {
       let inSight = (bubble.sprite.position.y < window.innerHeight + 11);
 
       // if (!inSight) {
       //   this.parent.stage.removeChild(bubble.sprite);
       // }
-      return inSight;
+      return (inSight && !bubble.popped);
     }
 
     update(timeStamp) {
+
+      this.scoreText.text = ('Score: ' + this.score);
+
       let numBubs = this.bubbles.length;
 
       for (let i = 0; i < numBubs; i++) {
         this.bubbles[i].update();
 
         // remove out of screen bubbles from screen
-        if (!this.inScreen(this.bubbles[i])) {
+        if (!this.isAlive(this.bubbles[i])) {
           this.parent.stage.removeChild(this.bubbles[i].sprite);
         }
       }
       // remove out of screen bubbles from array
-      this.bubbles = this.bubbles.filter(this.inScreen);
+      this.bubbles = this.bubbles.filter(this.isAlive);
 
       if (Math.random() < 0.2) {
         this.initBubble();
       }
+
     }
 
 }
