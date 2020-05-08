bb2393d82b3b29e7bb273adfc65ca2060a5416f3
Abigail Rettew <abigailrettew@Abigails-MacBook-Pro-2.local>
2020-05-08 15:43:16 -0400

    Added notebook to examine

---
+++ src/components/objects/Page/Page.js
@@ -0,0 +1,33 @@
+import {
+    MeshStandardMaterial,
+    PlaneBufferGeometry,
+    Mesh,
+    TextureLoader
+} from 'three';
+import TEXTURE from './page2.png';
+
+class Page extends Mesh {
+    constructor() {
+        const loader = new TextureLoader();
+        let texture = loader.load(TEXTURE);
+        const material = new MeshStandardMaterial({
+            color: 0xffe747,
+            map: texture,
+        });
+        const geometry = new PlaneBufferGeometry(1, 1);
+        super(geometry, material);
+
+        this.position.y = 1;
+        this.position.z = 1;
+        this.rotation.x = -.5;
+        this.scale.multiplyScalar(3);
+
+        this.visible = false;
+    }
+
+    setVisible(visible) {
+        this.visible = visible;
+    }
+}
+
+export default Page;
